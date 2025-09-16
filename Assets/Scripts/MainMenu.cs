7e44cba615fbda4258b0d18673e5974469ef465b
g-hasen <g.ahmedhasen@gmail.com>
2025-09-16 13:24:55 +0300

    commit6

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,23 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject howToPlayPanel;
-
     public void StartGame() {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("Level1"); 
     }
-
-    public void ShowHowToPlay() { 
-        howToPlayPanel.SetActive(true); 
-
-    }
-    public void CloseHowToPlay() { 
-        howToPlayPanel.SetActive(false);
-
-    }
     public void QuitGame() { 
         Application.Quit(); 
     }
+
+    //public GameObject howToPlayPanel;
+    //public void ShowHowToPlay() { 
+    //    howToPlayPanel.SetActive(true); 
+
+    //}
+    //public void CloseHowToPlay() { 
+    //    howToPlayPanel.SetActive(false);
+
+    //}
 }
